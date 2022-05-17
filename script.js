b47c7636d3aab8e4eb99aa4b364e44ab584142bf
kozhobekov16 <zhakshylykkozhobekov@gmail.com>
2022-05-17 19:22:08 +0600

    text

---
+++ script.js
@@ -0,0 +1,35 @@
+//Object
+
+        // let daniar = {
+        //     name: 'Daniar',
+        //     age: 20,
+        //     hobbes: {
+        //         fist: "play Football",
+        //         second: "Sing song",
+        //     }
+        // }
+
+        // console.log(Object.keys(daniar))
+        // console.log(Object.values(daniar))
+
+        //функции 
+
+        // let num = [10, 50, 1, 5]
+
+        // function mult(mairambek){
+        //     document.write(mairambek)
+        // }
+
+        // mult(num)
+
+
+let button = document.querySelector('.plus')
+button.addEventListener('mousemove', func)
+let p = document.getElementById('p')
+function func(){
+    p.style.transform.scale()
+}
+
+//mousemove
+
+//mouseout
